5fa8d3b72c579f38af54b366ed952f9e786feb31
SMVR2 <52793419+SMVR2@users.noreply.github.com>
2020-04-19 23:28:56 -0400

    Update db2.py

--- db2.py
+++ db2.py
@@ -4,6 +4,7 @@ import psycopg2
 import subprocess
 import os
 proc = subprocess.Popen('sudo heroku config:get DATABASE_URL -a sminfo', stdout=subprocess.PIPE, shell=True)
+#change the input after -a to your heroku app details
 db_url = proc.stdout.read().decode('utf-8').strip() + '?sslmode=require'
 
 conn = psycopg2.connect(db_url)
